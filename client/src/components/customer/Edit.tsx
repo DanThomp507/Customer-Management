089d8427068953ba557f719f213898662b66c7d2
Daniel Thompson <DanThomp507@gmail.com>
2019-06-20 11:12:21 -0400

    set up interface in edit.tsx

--- client/src/components/customer/Edit.tsx
+++ client/src/components/customer/Edit.tsx
@@ -1,3 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import axios from 'axios';
+
+export interface IValues {
+    [key: string]: any;
+}
